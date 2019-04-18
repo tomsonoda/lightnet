c20d807852967cf2edd04b1dee65872c65652c65
Tom Sonoda <tom.sonoda@gmal.com>
2019-04-19 01:28:54 +0900

    cuda

--- src/LayerLeakyReLUKernels.cu
+++ src/LayerLeakyReLUKernels.cu
@@ -3,17 +3,17 @@
 
 namespace gpu_cuda {
 
-dim3 cuda_gridsize(size_t n){
-  size_t k = (n-1) / BLOCK + 1;
-  size_t x = k;
-  size_t y = 1;
-  if(x > 65535){
-      x = ceil(sqrt(k));
-      y = (n-1)/(x*BLOCK) + 1;
-  }
-  //printf("%ld %ld %ld %ld\n", n, x, y, x*y*BLOCK);
-  return {x, y, 1};
-}
+// dim3 cuda_gridsize(size_t n){
+//   size_t k = (n-1) / BLOCK + 1;
+//   size_t x = k;
+//   size_t y = 1;
+//   if(x > 65535){
+//       x = ceil(sqrt(k));
+//       y = (n-1)/(x*BLOCK) + 1;
+//   }
+//   //printf("%ld %ld %ld %ld\n", n, x, y, x*y*BLOCK);
+//   return {x, y, 1};
+// }
 
 void calc(float *x, float *y)
 {
@@ -34,7 +34,9 @@ __global__ void leakyReluForwardGPU(float *data_in, float *data_out, int N)
   cudaMemcpy(d_in,  data_in,  N*sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(d_out, data_out, N*sizeof(float), cudaMemcpyHostToDevice);
 
-  calc<<<cuda_gridsize(N), BLOCK>>>(d_in, d_out);
+  dim3 block (BLOCK, 1, 1);
+  dim3 grid  (N / block.x, 1, 1);
+  calc<<<grid, BLOCK>>>(d_in, d_out);
 
   cudaMemcpy(data_out, d_out, N*sizeof(float), cudaMemcpyDeviceToHost);
 }
