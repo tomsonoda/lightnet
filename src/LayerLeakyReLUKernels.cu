43364b7641cd4513ba6b58d9c80a05b29b13cb7c
Tom Sonoda <tom.sonoda@gmal.com>
2019-04-21 13:07:31 +0900

    add Cuda.hpp

--- src/LayerLeakyReLUKernels.cu
+++ src/LayerLeakyReLUKernels.cu
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "Cuda.hpp"
 #define BLOCK 512
 
 namespace gpu_cuda {
@@ -22,19 +23,6 @@ __global__ void calcLeakyReluBackwardGPU(float *in1, float *in2, float *in3, flo
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   in3[i] += in2[i];
   out[i] +=  (in1[i] < 0) ? (0.1) : in3[i];
-}
-
-dim3 cudaGridSize(size_t n){
-    size_t k = (n-1) / BLOCK + 1;
-    size_t x = k;
-    size_t y = 1;
-    if(x > 65535){
-        x = ceil(sqrt(k));
-        y = (n-1)/(x*BLOCK) + 1;
-    }
-    dim3 d = {x, y, 1};
-    //printf("%ld %ld %ld %ld\n", n, x, y, x*y*BLOCK);
-    return d;
 }
 
 void leakyReluForwardGPU(float *data_in, float *data_out, int N)
