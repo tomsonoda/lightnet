652039fb16798966a5f0ec0dd0ff4d13de14bda7
Tom Sonoda <tom.sonoda@gmal.com>
2019-04-21 12:55:17 +0900

    cudaGridSize

--- src/LayerLeakyReLUKernels.cu
+++ src/LayerLeakyReLUKernels.cu
@@ -24,6 +24,19 @@ __global__ void calcLeakyReluBackwardGPU(float *in1, float *in2, float *in3, flo
   out[i] +=  (in1[i] < 0) ? (0.1) : in3[i];
 }
 
+dim3 cudaGridSize(size_t n){
+    size_t k = (n-1) / BLOCK + 1;
+    size_t x = k;
+    size_t y = 1;
+    if(x > 65535){
+        x = ceil(sqrt(k));
+        y = (n-1)/(x*BLOCK) + 1;
+    }
+    dim3 d = {x, y, 1};
+    //printf("%ld %ld %ld %ld\n", n, x, y, x*y*BLOCK);
+    return d;
+}
+
 void leakyReluForwardGPU(float *data_in, float *data_out, int N)
 {
   float *d_in, *d_out;
@@ -34,7 +47,7 @@ void leakyReluForwardGPU(float *data_in, float *data_out, int N)
   cudaMemcpy(d_out, data_out, N*sizeof(float), cudaMemcpyHostToDevice);
 
   dim3 block (BLOCK, 1, 1);
-  dim3 grid  (N / block.x, 1, 1);
+  dim3 grid  cudaGridSize(N);
   calcLeakyReluForwardGPU<<<grid, BLOCK>>>(d_in, d_out);
 
   cudaMemcpy(data_out, d_out, N*sizeof(float), cudaMemcpyDeviceToHost);
