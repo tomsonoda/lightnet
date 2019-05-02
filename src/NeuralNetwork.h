c1792be79e5907ea3e9bcf5e13ba35dbef7adb85
Tom Sonoda <tom.sonoda@gmal.com>
2019-05-02 15:34:43 +0900

    bug fix

--- src/NeuralNetwork.h
+++ src/NeuralNetwork.h
@@ -213,8 +213,8 @@ static float trainNetworkGPU(
 
 	for( int i = 0; i < layers.size(); ++i ){
 		printf("clear memory [%d] size:%d\n", i, layers[i]->dz_in.size.b * layers[i]->dz_in.size.x * layers[i]->dz_in.size.y * layers[i]->dz_in.size.z * sizeof( float ));
-		cudaClearArray( layers[i]->gpu_dz_in, layers[i]->dz_in.size.b * layers[i]->dz_in.size.x * layers[i]->dz_in.size.y * layers[i]->dz_in.size.z);
-		cudaClearArray( layers[i]->gpu_dz, layers[i]->dz.size.b * layers[i]->dz.size.x * layers[i]->dz.size.y * layers[i]->dz.size.z);
+		gpu_cuda::cudaClearArray( layers[i]->gpu_dz_in, layers[i]->dz_in.size.b * layers[i]->dz_in.size.x * layers[i]->dz_in.size.y * layers[i]->dz_in.size.z);
+		gpu_cuda::cudaClearArray( layers[i]->gpu_dz, layers[i]->dz.size.b * layers[i]->dz.size.x * layers[i]->dz.size.y * layers[i]->dz.size.z);
 	}
 
 	for ( int i = layers.size() - 1; i >= 0; i-- ){
