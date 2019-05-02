3d166b053a25e8fea7fe0d0f19ac3b7878c00021
Tom Sonoda <tom.sonoda@gmal.com>
2019-05-02 15:05:44 +0900

    __host__ call in Cuda.cu

--- src/Cuda.cu
+++ src/Cuda.cu
@@ -2,18 +2,18 @@
 
 namespace gpu_cuda {
 
-__device__ void cudaMakeArray(float *gpu_array, int N )
+void cudaMakeArray(float *gpu_array, int N )
 {
   cudaMalloc(&gpu_array, N*sizeof(float));
   cudaMemset(&gpu_array, 0, N*sizeof(float));
 }
 
-__device__ void cudaPutArray( float *gpu_array, float *cpu_array, int N )
+void cudaPutArray( float *gpu_array, float *cpu_array, int N )
 {
   cudaMemcpy(gpu_array, cpu_array, N*sizeof(float), cudaMemcpyHostToDevice);
 }
 
-__device__ void cudaGetArray( float *cpu_array, float *gpu_array, int N )
+void cudaGetArray( float *cpu_array, float *gpu_array, int N )
 {
   cudaMemcpy(cpu_array, gpu_array, N*sizeof(float), cudaMemcpyDeviceToHost);
 }
