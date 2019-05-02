ba942bf5da7164b810503b7f3322b35c48ca3b0d
Tom Sonoda <tom.sonoda@gmal.com>
2019-05-02 21:21:38 +0900

    update leaky relu gpu

--- src/LayerLeakyReLUKernels.cu
+++ src/LayerLeakyReLUKernels.cu
@@ -29,9 +29,9 @@ void leakyReluForwardGPU(float *in, float *out, int N )
 
 void leakyReluBackwardGPU( float *dz_next_layer, float *dz_in, float *dz, float *in, int N )
 {
-  for( int i = 0; i < N ; ++i ){
-    dz_in[i] += dz_next_layer[i];
-  }
+  // for( int i = 0; i < N ; ++i ){
+  //   dz_in[i] += dz_next_layer[i];
+  // }
 
   // CudaObject cuda = CudaObject();
   // dim3 grid = cuda.cudaGridSize(N);
