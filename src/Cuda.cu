49b040e9cc7b096e09bf058a520a121632175a19
Tom Sonoda <tom.sonoda@gmal.com>
2019-05-03 10:31:21 +0900

    update cuda.cu

--- src/Cuda.cu
+++ src/Cuda.cu
@@ -10,25 +10,10 @@ __device__ unsigned int Rand(unsigned int randx)
   return randx&2147483647;
 }
 
-__global__ void setRandom(float *gpu_array, int maxval )
-{
-  int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
-  gpu_array[id] = 1.0f / maxval * Rand(id) / float( RAND_MAX );
-}
-
 void cudaMakeArray(float *gpu_array, int N )
 {
   cudaMalloc(&gpu_array, N*sizeof(float));
   cudaMemset(&gpu_array, 0, N*sizeof(float));
-}
-
-void cudaMakeRandomArray(float *gpu_array, int N, int maxval )
-{
-  cudaMalloc(&gpu_array, N*sizeof(float));
-  cudaMemset(&gpu_array, 0, N*sizeof(float));
-  CudaObject cuda = CudaObject();
-  dim3 grid = cuda.cudaGridSize(N);
-  setRandom<<<grid, BLOCK>>>( gpu_array, maxval );
 }
 
 void cudaPutArray( float *gpu_array, float *cpu_array, int N )
@@ -43,6 +28,21 @@ void cudaGetArray( float *cpu_array, float *gpu_array, int N )
 
 void cudaClearArray( float *gpu_array, int N ){
   cudaMemset(&gpu_array, 0, N*sizeof(float));
+}
+
+__global__ void setRandom(float *gpu_array, int maxval )
+{
+  int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
+  gpu_array[id] = 1.0f / maxval * Rand(id) / float( RAND_MAX );
+}
+
+void cudaMakeRandomArray(float *gpu_array, int N, int maxval )
+{
+  cudaMalloc(&gpu_array, N*sizeof(float));
+  cudaMemset(&gpu_array, 0, N*sizeof(float));
+  CudaObject cuda = CudaObject();
+  dim3 grid = cuda.cudaGridSize(N);
+  setRandom<<<grid, BLOCK>>>( gpu_array, maxval );
 }
 
 } // namespace gpu
