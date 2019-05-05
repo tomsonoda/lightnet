eed517a671e085c2f56cbf4a76ed51f49dc785f1
Tom Sonoda <tom.sonoda@gmal.com>
2019-05-05 23:54:56 +0900

    cudaAddFirstArrayToSecondArray changed to device inline

--- src/CudaCommon.cuh
+++ src/CudaCommon.cuh
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #pragma once
-__global__ void cudaAddFirstArrayToSecondArray(float * dz_next_layer, float *dz_in)
+__device__ inline void cudaAddFirstArrayToSecondArray(float * dz_next_layer, float *dz_in)
 {
   int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
   dz_in[id] += dz_next_layer[id];
