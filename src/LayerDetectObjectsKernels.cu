444b7a2e1e9b9e2b379fda894e0cc4ebb123ce05
Tom Sonoda <tom.sonoda@gmal.com>
2019-05-05 23:45:35 +0900

    update detect objects cuda

--- src/LayerDetectObjectsKernels.cu
+++ src/LayerDetectObjectsKernels.cu
@@ -41,15 +41,15 @@ __global__ void calcDetectObjectsForwardGPU(float *in, float *out, int in_size_x
   */
 }
 
-__global__ void calcDetectObjectsBackwardGPU( float *dz_next_layer, float *dz_in, float *dz, float *in, int max_bounding_boxes, int max_classes )
+__global__ void calcDetectObjectsBackwardGPU( float *dz_next_layer, float *dz_in, float *dz, float *in, int batch_size, int in_size_x, int in_size_y, int in_size_z, int max_bounding_boxes, int max_classes )
 {
   int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
 
 	for( int i = 0; i < max_bounding_boxes; i=i+(4+max_classes)){
 		int index = id * (in_size_x * in_size_y * in_size_z) + i;
 
-		dz[index  ] = activator_derivative( in( b, i  , 0, 0 ) ) * dz_in( b, i  , 0, 0 ); // x: sigmoid derivative * grads
-		dz[index+1] = activator_derivative( in( b, i+1 , 0, 0 ) ) * dz_in( b, i+1, 0, 0 ); // y: sigmoid derivative * grads
+		dz[index  ] = activator_derivative( in[index  ] ) * dz_in[index  ]; // x: sigmoid derivative * grads
+		dz[index+1] = activator_derivative( in[index+1] ) * dz_in[index+1]; // y: sigmoid derivative * grads
 		dz[index+2] = exp( in[index+2] ) * dz_in[index+2]; // w: exp * grads
 		dz[index+3] = exp( in[index+3] ) * dz_in[index+3]; // w: exp * grads
 		for( int c = 0; c <max_classes; ++c){
@@ -91,7 +91,7 @@ void detectObjectsBackwardGPU( float *dz_in, float *dz, float *in, int batch_siz
 {
   CudaObject cuda = CudaObject();
   dim3 grid = cuda.cudaGridSize(batch_size);
-  calcDetectObjectsBackwardGPU<<<grid, BLOCK>>>( dz_next_layer, dz_in, dz, in, max_bounding_boxes, max_classes );
+  calcDetectObjectsBackwardGPU<<<grid, BLOCK>>>( dz_next_layer, dz_in, dz, in, batch_size, in_size_x, in_size_y, int in_size_z, max_bounding_boxes, max_classes );
 }
 
 } // namespace gpu
