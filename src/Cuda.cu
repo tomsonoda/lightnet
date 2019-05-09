5313ecb853fbf032c83066a1dcb28972397f5647
Tom Sonoda <tom.sonoda@gmal.com>
2019-05-09 20:36:19 +0900

    cuda check error

--- src/Cuda.cu
+++ src/Cuda.cu
@@ -14,18 +14,41 @@ float *cudaMakeArray( int N )
 {
   float *gpu_array;
   cudaMalloc((void **)&gpu_array, N*sizeof(float));
-  // cudaMemset(&gpu_array, 0, N*sizeof(float));
+  cudaMemset(&gpu_array, 0, N*sizeof(float));
   return gpu_array;
+}
+
+void cudaCheckError(cudaError_t status)
+{
+    cudaError_t status2 = cudaGetLastError();
+    if (status != cudaSuccess){
+        const char *s = cudaGetErrorString(status);
+        char buffer[256];
+        printf("CUDA Error: %s\n", s);
+        assert(0);
+        snprintf(buffer, 256, "CUDA Error: %s", s);
+        error(buffer);
+    }
+    if (status2 != cudaSuccess){
+        const char *s = cudaGetErrorString(status);
+        char buffer[256];
+        printf("CUDA Error Prev: %s\n", s);
+        assert(0);
+        snprintf(buffer, 256, "CUDA Error Prev: %s", s);
+        error(buffer);
+    }
 }
 
 void cudaPutArray( float *gpu_array, float *cpu_array, int N )
 {
-  cudaMemcpy(gpu_array, cpu_array, N*sizeof(float), cudaMemcpyHostToDevice);
+  cudaError_t status = cudaMemcpy(gpu_array, cpu_array, N*sizeof(float), cudaMemcpyHostToDevice);
+  check_error(status);
 }
 
 void cudaGetArray( float *cpu_array, float *gpu_array, int N )
 {
-  cudaMemcpy(cpu_array, gpu_array, N*sizeof(float), cudaMemcpyDeviceToHost);
+  cudaError_t status = cudaMemcpy(cpu_array, gpu_array, N*sizeof(float), cudaMemcpyDeviceToHost);
+  check_error(status);
 }
 
 void cudaClearArray( float *gpu_array, int N )
