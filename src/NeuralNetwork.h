7493ac49d133c596f16401a61ec331c68bb70d39
Tom Sonoda <tom.sonoda@gmal.com>
2019-05-09 19:53:13 +0900

    debug gpu

--- src/NeuralNetwork.h
+++ src/NeuralNetwork.h
@@ -284,7 +284,7 @@ static float testNetworkGPU(
 			forwardGPU( layers[i], gpu_array );
 		}else{
 			int out_size = layers[i-1]->out.size.b * layers[i-1]->out.size.x * layers[i-1]->out.size.y * layers[i-1]->out.size.z;
-			TensorObject<float> gpu_output_data = TensorObject<float>(out_size.size.b, out_size.size.x, out_size.size.y, out_size.size.z);
+			TensorObject<float> gpu_output_data = TensorObject<float>(layers[i-1]->out.size.b, layers[i-1]->out.size.x, layers[i-1]->out.size.y, layers[i-1]->out.size.z);
 			gpu_cuda::cudaGetArray( gpu_output_data.data, layers.back()->gpu_out, out_size );
 			printTensor( gpu_output_data );
 			forwardGPU( layers[i], layers[i-1]->gpu_out );
