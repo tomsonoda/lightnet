ed343824245f2c254eb0ef2d69489b38860ae8da
Tom Sonoda <tom.sonoda@gmal.com>
2019-05-19 10:19:24 +0900

    memcheck dense

--- src/LayerDenseKernels.cu
+++ src/LayerDenseKernels.cu
@@ -10,29 +10,33 @@ __global__ void calcDenseForwardGPU( float *in, float *out, float *weights, floa
   int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
   int id_out = id;
 
-  int n = id % out_size_x;
-  id /= out_size_x;
-  // int y = id % out_size_y;
-  id /= out_size_y;
-  // int z = id % out_size_z;
-  id /= out_size_z;
-  int b = id;
 
-  int w_size_x = in_size_x*in_size_y*in_size_z;
+  if ( id_out < batch_size * out_size_x * out_size_y * out_size_z ){
+    int n = id % out_size_x;
+    id /= out_size_x;
+    // int y = id % out_size_y;
+    id /= out_size_y;
+    // int z = id % out_size_z;
+    id /= out_size_z;
+    int b = id;
 
-  float sum = 0;
-  for ( int k = 0; k < in_size_z; ++k ){
-    for ( int j = 0; j < in_size_y; ++j ){
-      for ( int i = 0; i < in_size_x; ++i ){
-        int m = k * (in_size_x * in_size_y) + j * (in_size_x) + i;
-        int w_index = n * (w_size_x) + m;
-        int in_index = b * (in_size_x * in_size_y * in_size_z) + k * (in_size_x * in_size_y) + j * in_size_x + i;
-        sum += in[in_index] * weights[w_index];
+    int w_size_x = in_size_x*in_size_y*in_size_z;
+
+    float sum = 0;
+    for ( int k = 0; k < in_size_z; ++k ){
+      for ( int j = 0; j < in_size_y; ++j ){
+        for ( int i = 0; i < in_size_x; ++i ){
+          int m = k * (in_size_x * in_size_y) + j * (in_size_x) + i;
+          int w_index = n * (w_size_x) + m;
+          int in_index = b * (in_size_x * in_size_y * in_size_z) + k * (in_size_x * in_size_y) + j * in_size_x + i;
+          sum += in[in_index] * weights[w_index];
+        }
       }
     }
+    int bias_index = n;
+    out[id_out] = sum + biases[bias_index];
   }
-  int bias_index = n;
-  out[id_out] = sum + biases[bias_index];
+
   /*
   for ( int b = 0; b < in.size.b; ++b ){
     for ( int n = 0; n < out.size.x; ++n ){
