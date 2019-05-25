6cf7c5269dae70f2a273560d73bd4a59e2b047df
Tom Sonoda <tom.sonoda@gmal.com>
2019-05-26 01:31:54 +0900

    softmax reduction.

--- src/LayerSoftmaxKernels.cu
+++ src/LayerSoftmaxKernels.cu
@@ -3,6 +3,88 @@
 
 namespace gpu_cuda {
 
+__global__ void calcSoftmaxMaxForwardGPU(float *in, float *odata, int batch_size, int in_size_x)
+{
+  extern __shared__ int sdata[];
+  int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
+  unsigned int tid = threadIdx.x;
+  sdata[tid] = (id < batch_size * in_size_x) ? in[id] : 0;
+
+  if(id<batch_size * in_size_x){
+    __syncthreads();
+    for (unsigned int s=1; s<blockDim.x; s*=2) {
+      if (tid % (2*s) == 0) {
+        sdata[tid] =  sdata[tid] > sdata[tid + s] ? sdata[tid] : sdata[tid + s];
+      }
+      __syncthreads();
+    }
+  }
+
+  if (tid == 0){
+    odata[0] = sdata[0];
+  }
+  /* original
+  for ( int b = 0; b < in.size.b; ++b ){
+    float max_v = 0.0;
+    for ( int i = 0; i < in.size.x; ++i ){
+      float v = in( b, i, 0, 0 );
+      if(v>max_v){
+        max_v = v;
+      }
+    }
+  }
+  */
+}
+
+__global__ void calcSoftmaxSumForwardGPU(float *in, float *out, float *odata, int batch_size, int in_size_x)
+{
+  // int blockID  = blockIdx.x;
+  // int nBlocks  = gridDim.x;
+  // int threadID = threadIdx.x;
+  // int nThrads  = blockDim.x;
+
+  extern __shared__ int sdata[];
+  int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
+  unsigned int tid = threadIdx.x;
+  sdata[tid] = (id < batch_size * in_size_x) ? in[id] : 0;
+
+  if(id<batch_size * in_size_x){
+    __syncthreads();
+    for (unsigned int s=1; s<blockDim.x; s*=2) {
+      if (tid % (2*s) == 0) {
+        sdata[tid] += expf( sdata[tid + s] - odata[0] );
+      }
+      __syncthreads();
+    }
+  }
+
+  out[id] = expf( in[id] - odata[0] );
+
+  // sum.
+  if (tid == 0){
+    odata[0] = sdata[0];
+  }
+
+  /* original
+  float sum = 0.0;
+  for ( int i = 0; i < in.size.x; ++i ){
+    float v = in( b, i, 0, 0 );
+    v = exp(v - max_v);
+    out( b, i, 0, 0 ) = v;
+    sum += v;
+  }
+  */
+}
+
+__global__ void calcSoftmaxDivForwardGPU(float *out, float *odata, int batch_size, int in_size_x)
+{
+  int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
+  if(id<batch_size * in_size_x && odata[0]>0.0){
+    out[id] = out[id] / odata[0];
+  }
+}
+
+
 __global__ void calcSoftmaxForwardGPU(float *in, float *out, int batch_size, int in_size_x)
 {
   // int blockID  = blockIdx.x;
@@ -20,6 +102,7 @@ __global__ void calcSoftmaxForwardGPU(float *in, float *out, int batch_size, int
         max_v = v;
       }
     }
+
     float sum = 0.0;
     for ( int i = 0; i < in_size_x; ++i ){
       float v = in[id + i];
@@ -27,6 +110,7 @@ __global__ void calcSoftmaxForwardGPU(float *in, float *out, int batch_size, int
       out[id + i] = v;
       sum += v;
     }
+
     if (sum > 0.0){
       for ( int i = 0; i < in_size_x; ++i ){
         float v = out[id + i] / sum;
@@ -48,7 +132,6 @@ __global__ void calcSoftmaxForwardGPU(float *in, float *out, int batch_size, int
     }
 
     float sum = 0.0;
-
     for ( int i = 0; i < in.size.x; ++i ){
       float v = in( b, i, 0, 0 );
       v = exp(v - max_v);
@@ -83,10 +166,16 @@ __global__ void calcSoftmaxBackwardGPU( float *dz_next_layer, float *dz_in, floa
 
 void softmaxForwardGPU( float *in, float *out, int batch_size, int in_size_x )
 {
-  // CudaObject cuda = CudaObject();
-  // dim3 grid = cuda.cudaGridSize( batch_size );
+  CudaObject cuda = CudaObject();
+  dim3 grid = cuda.cudaGridSize( batch_size * in_size_x );
+
+  float *odata
+  cudaMalloc( (void **)&odata, sizeof(float));
+  calcSoftmaxMaxForwardGPU<<<grid, BLOCK>>>( in, odata, batch_size, in_size_x );
+  calcSoftmaxSumForwardGPU<<<grid, BLOCK>>>( in, out, odata, batch_size, in_size_x );
+  calcSoftmaxDivForwardGPU<<<grid, BLOCK>>>( out, odata, batch_size, in_size_x );
+  
   // calcSoftmaxForwardGPU<<<grid, BLOCK>>>( in, out, batch_size, in_size_x );
-  calcSoftmaxForwardGPU<<<1, 1>>>( in, out, batch_size, in_size_x );
 }
 
 void softmaxBackwardGPU( float *dz_next_layer, float *dz_in, float *dz, int N )
