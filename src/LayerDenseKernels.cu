d8d1befb8b0d4234b309b274b1b4d6230b7a689b
Tom Sonoda <tom.sonoda@gmal.com>
2019-06-06 16:57:21 +0900

    update

--- src/LayerDenseKernels.cu
+++ src/LayerDenseKernels.cu
@@ -57,20 +57,25 @@ __global__ void calcDenseForwardGPU( float *in, float *out, float *weights, floa
 __global__ void calcDenseUpdateWeightsGPU( float *weights, float *biases, float *gradients, float *dW, float *dB, float learning_rate, int batch_size, int in_size_x, int in_size_y, int in_size_z, int out_size_x, int out_size_y, int out_size_z, int momentum )
 {
   int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
-  int w_size_x = in_size_x*in_size_y*in_size_z;
-  int w_size_y = out_size_x;
 
-  for(int h=0; h<w_size_x; ++h){
-      int index = id * (w_size_x * w_size_y) + h;
-      weights[index] = weights[index] - learning_rate * 	dW[index];
+  if ( id < out_size_x ) {
+    int w_size_x = in_size_x*in_size_y*in_size_z;
+    int w_size_y = out_size_x;
+
+    for(int h=0; h<w_size_x; ++h){
+        int index = id * (w_size_x * w_size_y) + h;
+        weights[index] = weights[index] - learning_rate * 	dW[index];
+    }
+
+    biases[id] = biases[id] - learning_rate * dB[id];
+
+    for( int b=0; b<batch_size; b++ ){
+      int index = (b * out_size_x + id) * 2;
+      gradients[index+1] = gradients[index] + gradients[index+1] * momentum;
+    }
+    
   }
 
-  biases[id] = biases[id] - learning_rate * dB[id];
-
-  for( int b=0; b<batch_size; b++ ){
-    int index = (b * out_size_x + id) * 2;
-    gradients[index+1] = gradients[index] + gradients[index+1] * momentum;
-  }
   /*
   for (int i=0; i<weigts_data_num; ++i){
     weights.data[i] = weights.data[i] - lr * 	dW.data[i];
