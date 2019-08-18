83d5e3163425eb9da41ada5144022a27d4a08d0b
tomsonoda <tom.sonoda@gmail.com>
2019-08-19 06:30:59 +0900

    Leaky ReLU for CUDA completed.

--- src/LayerLeakyReLUKernels.cu
+++ src/LayerLeakyReLUKernels.cu
@@ -3,36 +3,57 @@
 
 namespace gpu_cuda {
 
-__global__ void calcLeakyReluForwardGPU(float *in, float *out)
+__global__ void calcLeakyReluForwardGPU(float *in, float *out, int elements)
 {
-  // int i = blockIdx.x*blockDim.x + threadIdx.x;
   int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
-  float v = in[id];
-  if ( v < 0 ){
-    v = 0.01;
+  if( id < elements ){
+    float v = in[id];
+    if ( v < 0 ){
+      v = 0.01;
+    }
+    out[id] = v;
   }
-  out[id] = v;
+
+  /* original
+  for( unsigned i = 0; i < data_size; ++i ){
+    float v = in.data[i];
+    if ( v < 0 ){
+      v = 0.01;
+    }
+    out.data[i] = v;
+  }
+  */
 }
 
-__global__ void calcLeakyReluBackwardGPU( float *dz_next_layer, float *dz_in, float *dz, float *in )
+__global__ void calcLeakyReluBackwardGPU( float *dz_next_layer, float *dz_in, float *dz, float *in, int elements )
 {
   int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
-  dz_in[id] += dz_next_layer[id];
-  dz[id] +=  (in[id] < 0) ? (0.01) : (dz_in[id]);
+
+  if( id < elements ){
+    dz_in[id] += dz_next_layer[id];
+    dz[id] += (in[id] < 0) ? (0.01) : (dz_in[id]);
+  }
+
+  /* original
+  for( unsigned i = 0; i < data_size; ++i ){
+    dz_in.data[i] += dz_next_layer.data[i];
+    dz.data[i] +=  (in.data[i] < 0) ? (0.01) : (1.0 * dz_in.data[i]);
+  }
+  */
 }
 
-void leakyReluForwardGPU(float *in, float *out, int N )
+void leakyReluForwardGPU(float *in, float *out, int N)
 {
   CudaObject cuda = CudaObject();
   dim3 grid = cuda.cudaGridSize(N);
-  calcLeakyReluForwardGPU<<<grid, BLOCK>>>(in, out);
+  calcLeakyReluForwardGPU<<<grid, BLOCK>>>(in, out, N);
 }
 
 void leakyReluBackwardGPU( float *dz_next_layer, float *dz_in, float *dz, float *in, int N )
 {
   CudaObject cuda = CudaObject();
   dim3 grid = cuda.cudaGridSize(N);
-  calcLeakyReluBackwardGPU<<<grid, BLOCK>>>( dz_next_layer, dz_in, dz, in );
+  calcLeakyReluBackwardGPU<<<grid, BLOCK>>>( dz_next_layer, dz_in, dz, in, N );
 }
 
 } // namespace gpu
